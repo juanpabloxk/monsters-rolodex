7fd448a8f9680b41677e19f0bde6e197e23f337b
xleed-dev <xleed@protonmail.ch>
2021-05-16 08:56:32 -0500

    Initial steps in monsters card views

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react'
+import { CardList } from "./components/card-list/card-list";
 
 class App extends Component{
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {name: 'Frankenstein'},
-        {name: 'Dracula'},
-        {name: 'Zombie'}
-      ]
+      monsters: []
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
   }
 
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Hello world!
-      </header>
+        <CardList monsters={this.state.monsters} />
+        
     </div>
     )
   }
